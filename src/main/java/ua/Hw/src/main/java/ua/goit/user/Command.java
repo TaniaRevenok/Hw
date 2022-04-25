e8d9de3c7f48e497d9021d12a5f7ed7ad56d71a7
TR <revenok16@gmail.com>
2022-04-25 16:00:35 +0300

    Git commit

---
+++ src/main/java/ua/Hw/src/main/java/ua/goit/user/Command.java
@@ -0,0 +1,13 @@
+package ua.goit.user;
+
+import java.io.IOException;
+
+public interface Command {
+    void process() throws IOException;
+
+    String commandName();
+
+    default boolean canProcess(String command) {
+        return commandName().equals(command);
+    }
+}
